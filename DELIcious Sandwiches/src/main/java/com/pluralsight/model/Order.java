da5bce824a4cb789b74182da6a18c82286e119b2
Gai'Nicholas Patton <nickpatton1999@yahoo.com>
2025-05-29 16:10:09 -0400

    updated order class

--- DELIcious Sandwiches/src/main/java/com/pluralsight/model/Order.java
+++ DELIcious Sandwiches/src/main/java/com/pluralsight/model/Order.java
@@ -1,4 +1,15 @@
 package com.pluralsight.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Order {
+
+    private List<Sandwich> sandwiches;
+    private int drinkSize;
+    private boolean chips;
+    private Date orderTime;
+
 }
