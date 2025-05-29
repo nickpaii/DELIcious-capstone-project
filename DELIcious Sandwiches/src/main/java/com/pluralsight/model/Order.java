3af33e4339e44a2a243811ab89ffac706499ecd6
Gai'Nicholas Patton <nickpatton1999@yahoo.com>
2025-05-29 16:18:20 -0400

    updated order class logic

--- DELIcious Sandwiches/src/main/java/com/pluralsight/model/Order.java
+++ DELIcious Sandwiches/src/main/java/com/pluralsight/model/Order.java
@@ -8,8 +8,23 @@ import java.util.Date;
 public class Order {
 
     private List<Sandwich> sandwiches;
-    private int drinkSize;
+    private int drinkSize; // 0 - none, 1 - small, 2 - medium, 3 - large
     private boolean chips;
     private Date orderTime;
 
+    public Order(List<Sandwich> sandwiches, Date orderTime, boolean chips, int drinkSize) {
+        this.sandwiches = sandwiches;
+        this.orderTime = orderTime;
+        this.chips = chips;
+        this.drinkSize = drinkSize;
+    }
+
+
+    public void setChips(boolean chips) {
+        this.chips = chips;
+    }
+
+    public void setDrinkSize(int size) {
+        this.drinkSize = size;
+    }
 }
